0f5871e917bb9bf98fe6fa0dfca4b97bf2b3f897
William Dines <williamdines@rocketmail.com>
2024-04-27 03:18:28 -0500

    import badge + card, begin about description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,40 +4,31 @@ import bonk from '../../public/next.svg';
 
 export default function Home() {
   return (
-    <div className=" h-screen bg-devLaptop bg-cover bg-bo ">
-      <div className="text-white flex justify-center pt-10 pl-5">
-        <div className="backdrop-blur-md backdrop-brightness-80 flex">
-          <h1 className="text-7xl">
+    <div className="w-screen h-screen bg-slate-900 sm:bg-devLaptop bg-cover bg-top">
+      <div className="backdrop-blur-md md:grid md:grid-flow-col">
+        <div className="py-5 flex flex-col pt-10 md:justify-center md:pt-20">
+          <h1 className="text-6xl text-slate-100 font-bold text-center sm:text-slate-950">
             William Dines
           </h1>
+          <p className="text-center text-xl text-slate-400">
+            Software Developer
+          </p>
+          <br />
+          <h1 className="text-center text-4xl font-black text-slate-100 sm:text-slate-950">
+            About
+          </h1>
+          <p className="text-2xl text-slate-100 font-bold text-center sm:text-slate-950">
+          Hi there! I'm William Dines, a dedicated software developer with a knack for turning ideas into reality through code.<br /><br />
+          I began my journey in 2019 when I switched my major from kinesiology to computer science at LSU<br /><br />
+          Since then, I've made countless pet projects, covering disciplines ranging from web scraping, to web development, to data science.<br /><br />
+          Let's connect and build something remarkable!
+          </p>
         </div>
-      </div>
-      <br />
-      <div className="flex justify-center w-full h-screen">
-        <Carousel className="">
-          <CarouselContent className="flex">
-            <CarouselItem className="flex justify-center">
-              <Avatar>
-                <AvatarImage src={bonk.src}/>
-                <AvatarFallback>Cn</AvatarFallback>
-              </Avatar>
-            </CarouselItem>
-            <CarouselItem className="flex justify-center">
-            <Avatar>
-                <AvatarImage src={bonk.src}/>
-                <AvatarFallback>Cn</AvatarFallback>
-              </Avatar>
-            </CarouselItem>
-            <CarouselItem className="flex justify-center">
-            <Avatar>
-                <AvatarImage src={bonk.src}/>
-                <AvatarFallback>Cn</AvatarFallback>
-              </Avatar>
-            </CarouselItem>
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+        <div className="flex justify-center w-full h-screen">
+          <p>
+            test
+          </p>
+        </div>
       </div>
     </div>
   );
