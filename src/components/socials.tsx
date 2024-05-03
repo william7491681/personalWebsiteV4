293f41f7530ca2dc35cb7c2664c7f9cb63ec747e
William Dines <williamdines@rocketmail.com>
2024-05-03 00:20:54 -0500

    updated socials

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Socials = () => {
   return (
     <div className='flex m-auto'>
-      <a href="https://www.linkedin.com/in/william-dines/" target="_blank" rel="noopener noreferrer" className='mr-5'>
-        <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" width="25" height="25" viewBox="0 0 310 310" className='fill-[#0077B5] sm:fill-black sm:hover:fill-[#0077B5]'>
+      <a href="https://www.linkedin.com/in/william-dines/" target="_blank" rel="noopener noreferrer" className='mr-5' title='Linkedin'>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" width="25" height="25" viewBox="0 0 310 310" className='fill-[#0077B5] md:hover:scale-125 transition ease-in'>
           <path d="M72.16 99.73H9.927a5 5 0 0 0-5 5v199.928a5 5 0 0 0 5 5H72.16a5 5 0 0 0 5-5V104.73a5 5 0 0 0-5-5zM41.066.341C18.422.341 0 18.743 0 41.362 0 63.991 18.422 82.4 41.066 82.4c22.626 0 41.033-18.41 41.033-41.038C82.1 18.743 63.692.341 41.066.341zM230.454 94.761c-24.995 0-43.472 10.745-54.679 22.954V104.73a5 5 0 0 0-5-5h-59.599a5 5 0 0 0-5 5v199.928a5 5 0 0 0 5 5h62.097a5 5 0 0 0 5-5V205.74c0-33.333 9.054-46.319 32.29-46.319 25.306 0 27.317 20.818 27.317 48.034v97.204a5 5 0 0 0 5 5H305a5 5 0 0 0 5-5V194.995c0-49.565-9.451-100.234-79.546-100.234z"/>
         </svg>
       </a>
-      <a href="https://www.github.com/william7491681" target="_blank" rel="noopener noreferrer">
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 24 24" className='fill-[#fafbfc] sm:fill-black sm:hover:fill-[#fafbfc]'>
-          <path stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16 22.027v-2.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 0 0-1.5-3.75 5.07 5.07 0 0 0-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 0 0-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 0 0 5 5.797a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 0 0-.94 2.58v2.87"/>
-          <path stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 20.027c-3 .973-5.5 0-7-3"/>
+      <a href="https://www.github.com/william7491681" target="_blank" rel="noopener noreferrer" title='Github'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 24 24" className='fill-[#fafbfc] scale-110 md:hover:scale-[1.4] transition ease-in'>
+          <path stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M16 22.027v-2.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 0 0-1.5-3.75 5.07 5.07 0 0 0-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 0 0-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 0 0 5 5.797a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 0 0-.94 2.58v2.87"/>
+          <path stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M9 20.027c-3 .973-5.5 0-7-3"/>
         </svg>
       </a>
     </div>
